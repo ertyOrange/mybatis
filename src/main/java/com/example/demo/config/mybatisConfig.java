2944b66e61751072dc36e373e3b38194dee353a3
ertyOrange <263374542@qq.com>
2019-05-15 01:03:45 +0800

    add config

---
+++ src/main/java/com/example/demo/config/mybatisConfig.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+/**
+ * @author yy263
+ *
+ */
+package com.example.demo.config;
+
+
+public class mybatisConfig {
+	
+//	public static void main(String[] args) throws IOException {
+//		getConfig();
+//	}
+//	public static void getConfig() throws IOException{
+//		String resource = "mybatis-config.xml";
+//		InputStream inputStream = Resources.getResourceAsStream(resource);
+//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+//	}
+}
