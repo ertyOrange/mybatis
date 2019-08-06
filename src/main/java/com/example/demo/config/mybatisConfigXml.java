6a7615f20c4e81702b2d107e5c4fe70f858b9511
ertyOrange <263374542@qq.com>
2019-08-06 22:39:02 +0800

    java  and xml

--- src/main/java/com/example/demo/config/mybatisConfigXml.java
+++ src/main/java/com/example/demo/config/mybatisConfigXml.java
@@ -7,15 +7,21 @@
  */
 package com.example.demo.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 
-public class mybatisConfig {
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class mybatisConfigXml {
 	
-//	public static void main(String[] args) throws IOException {
-//		getConfig();
-//	}
-//	public static void getConfig() throws IOException{
-//		String resource = "mybatis-config.xml";
-//		InputStream inputStream = Resources.getResourceAsStream(resource);
-//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-//	}
+	public static void main(String[] args) throws IOException {
+		getConfig();
+	}
+	public static void getConfig() throws IOException{
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
 }
